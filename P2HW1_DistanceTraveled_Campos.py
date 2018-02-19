ebe10307c47861c48b080b3c3ee74b148fd14b10
Alejandro Campos <36647702+camposalex1@users.noreply.github.com>
2018-02-19 17:20:52 -0500

    Add files via upload

---
+++ P2HW1_DistanceTraveled_Campos.py
@@ -0,0 +1,37 @@
+#A car is traveling at 70 miles per hour. Display the following:
+#•	The distance the car will travel in 6 hours
+#•	The distance the car will travel in 10 hours
+#•	The distance the car will travel in 15 hours
+
+
+
+
+
+
+print ("Distance traveled in 6 hour at 70 miles per hour" )
+
+speed = 70
+
+time = 6
+distance = speed * time
+print("Speed","time","Distance", sep="  ")
+print("---------------------")
+print(speed,'\t',time,'\t',distance)
+
+
+print ("Distance traveled in 10 hours at 70 miles per hour" )
+
+time = 10
+distance = speed * time
+print("Speed","time","Distance", sep="  ")
+print("---------------------")
+print(speed,'\t',time,'\t',distance)
+
+
+print ("Distance traveled in 15 hours at 70 miles per hour" )
+
+time = 15
+distance = speed * time
+print("Speed","time","Distance", sep="  ")
+print("---------------------")
+print(speed,'\t',time,'\t',distance)
