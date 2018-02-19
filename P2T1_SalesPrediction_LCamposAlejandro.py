2ccf05111356e6acd45a6f10d72b1a602db31f06
Alejandro Campos <36647702+camposalex1@users.noreply.github.com>
2018-02-19 17:17:27 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_LCamposAlejandro.py
@@ -0,0 +1,10 @@
+
+#Get the projected total sales.
+total_sales=float(input ("Enter the projected sales:"))
+
+# Calculate the profit as 23 percent of total sales.
+profit =total_sales * 0.23
+
+#Display the profit.
+print ("the profit is $", format (profit, ",.2f"))
+
